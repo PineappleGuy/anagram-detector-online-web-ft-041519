2b0623bd5077b48ccc7e05f0c81a5aab3ecadcf9
Garth Lilly <garff247@gmail.com>
2019-05-05 00:09:19 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :match
+  @@all = []
+
+  def initialize(string)
+    @string = string
+  end
+
+  def match(array)
+    array.each do |word|
+      word.split("")
+    end
+puts array
+    end
+
+
+
+
+end
