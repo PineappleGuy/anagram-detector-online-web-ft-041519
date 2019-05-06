769c6834425fb779f0ab887e066e364988be3ba7
Garth Lilly <garff247@gmail.com>
2019-05-06 16:11:24 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,20 +1,21 @@
 # Your code goes here!
+require 'pry'
 class Anagram
   attr_accessor :match
   @@all = []
-
+  @@some = []
   def initialize(string)
-    @string = string
+    @string = string.split("")
   end
 
   def match(array)
     array.each do |word|
-      word.split("")
+      @@all = word.split("").sort
     end
-puts array
+    if @@all == @string
+      @@all
     end
-
-
+  end
 
 
 end
